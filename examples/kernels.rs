15d1075c30a3023a49674f9ea3279a1ff4a10ddb
Corey Lowman <clowman1993@gmail.com>
2022-09-17 16:03:53 -0400

    Updating example

--- examples/kernels.rs
+++ examples/kernels.rs
@@ -130,8 +130,8 @@ extern \"C\" __global__ void kernel(float *out, const float *inp, int numel) {{
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let gpu = CudaDeviceBuilder::new(0)
-        .with_nvrtc_module(SinOp::NAME, ForEach::<SinOp>::compile()?, &["kernel"])
-        .with_nvrtc_module(CosOp::NAME, ForEach::<CosOp>::compile()?, &["kernel"])
+        .with_nvrtc_ptx(SinOp::NAME, ForEach::<SinOp>::compile()?, &["kernel"])
+        .with_nvrtc_ptx(CosOp::NAME, ForEach::<CosOp>::compile()?, &["kernel"])
         .build()
         .unwrap();
 
