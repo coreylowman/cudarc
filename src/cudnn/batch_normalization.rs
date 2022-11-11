94dd49c957c127378f2eb400ca022dc263624efe
M1ngXU <max.obreiter@gmail.com>
2022-11-11 11:53:55 +0100

    removed tensors from batch norm struct

--- src/cudnn/batch_normalization.rs
+++ src/cudnn/batch_normalization.rs
@@ -12,52 +12,46 @@ pub struct BatchNormalizationBackward<
     const H: usize,
     const W: usize,
 > {
-    cudnn_handle: Rc<CudnnHandle>,
-    x: Tensor4D<T, N, C, H, W>,
-    dx: Tensor4D<T, N, C, H, W>,
-    dy: Tensor4D<T, N, C, H, W>,
-    dummy_diff: Tensor4D<T, 1, C, 1, 1>,
-    saved_mean: Tensor4D<T, 1, C, 1, 1>,
+    dummy_diff:     Tensor4D<T, 1, C, 1, 1>,
+    saved_mean:     Tensor4D<T, 1, C, 1, 1>,
     saved_variance: Tensor4D<T, 1, C, 1, 1>,
 }
 impl<T: TensorDataType, const N: usize, const C: usize, const H: usize, const W: usize>
     BatchNormalizationBackward<T, N, C, H, W>
 {
     pub fn create(
-        cudnn_handle: Rc<CudnnHandle>,
-        x: Tensor4D<T, N, C, H, W>,
-        dx: Tensor4D<T, N, C, H, W>,
-        dy: Tensor4D<T, N, C, H, W>,
         dummy_diff: Tensor4D<T, 1, C, 1, 1>,
         saved_mean: Tensor4D<T, 1, C, 1, 1>,
         saved_variance: Tensor4D<T, 1, C, 1, 1>,
     ) -> Self {
         Self {
-            cudnn_handle,
-            x,
-            dx,
-            dy,
             dummy_diff,
             saved_mean,
             saved_variance,
         }
     }
 
-    pub fn execute(&self) -> CudnnResult<()> {
+    pub fn execute(
+        &self,
+        cudnn_handle: &CudnnHandle,
+        x: &Tensor4D<T, N, C, H, W>,
+        dx: &mut Tensor4D<T, N, C, H, W>,
+        dy: &Tensor4D<T, N, C, H, W>,
+    ) -> CudnnResult<()> {
         unsafe {
             cudnnBatchNormalizationBackward(
-                self.cudnn_handle.0,
+                cudnn_handle.0,
                 MODE,
                 &T::ONE as *const _ as *const _,
                 &T::ZERO as *const _ as *const _,
                 &T::ZERO as *const _ as *const _,
                 &T::ONE as *const _ as *const _,
-                self.x.descriptor.0,
-                self.x.data.t_cuda.cu_device_ptr as *const _,
-                self.dy.descriptor.0,
-                self.dy.data.t_cuda.cu_device_ptr as *const _,
-                self.dx.descriptor.0,
-                self.dx.data.t_cuda.cu_device_ptr as *mut _,
+                x.descriptor.0,
+                x.data.t_cuda.cu_device_ptr as *const _,
+                dy.descriptor.0,
+                dy.data.t_cuda.cu_device_ptr as *const _,
+                dx.descriptor.0,
+                dx.data.t_cuda.cu_device_ptr as *mut _,
                 self.dummy_diff.descriptor.0,
                 self.dummy_diff.data.t_cuda.cu_device_ptr as *const _,
                 self.dummy_diff.data.t_cuda.cu_device_ptr as *mut _,
@@ -77,9 +71,6 @@ pub struct BatchNormalizationForward<
     const H: usize,
     const W: usize,
 > {
-    cudnn_handle: Rc<CudnnHandle>,
-    x: Tensor4D<T, N, C, H, W>,
-    y: Tensor4D<T, N, C, H, W>,
     bias: Tensor4D<T, 1, C, 1, 1>,
     scale: Tensor4D<T, 1, C, 1, 1>,
     saved_mean: Tensor4D<T, 1, C, 1, 1>,
@@ -90,22 +81,14 @@ pub struct BatchNormalizationForward<
 impl<T: TensorDataType, const N: usize, const C: usize, const H: usize, const W: usize>
     BatchNormalizationForward<T, N, C, H, W>
 {
-    pub fn create(
-        device: &Rc<CudaDevice>,
-        cudnn_handle: Rc<CudnnHandle>,
-        x: Tensor4D<T, N, C, H, W>,
-        y: Tensor4D<T, N, C, H, W>,
-    ) -> CudaCudnnResult<Self> {
-        let bias = Tensor4D::alloc_all_same(device, &cudnn_handle, &T::ZERO)?;
-        let scale = Tensor4D::alloc_all_same(device, &cudnn_handle, &T::ONE)?;
+    pub fn create(device: &Rc<CudaDevice>, cudnn_handle: &CudnnHandle) -> CudaCudnnResult<Self> {
+        let bias = Tensor4D::alloc_all_same(device, cudnn_handle, &T::ZERO)?;
+        let scale = Tensor4D::alloc_all_same(device, cudnn_handle, &T::ONE)?;
         let saved_mean = unsafe { Tensor4D::alloc_uninit(device) }?;
         let saved_variance = unsafe { Tensor4D::alloc_uninit(device) }?;
-        let running_mean = Tensor4D::alloc_all_same(device, &cudnn_handle, &T::ZERO)?;
-        let running_variance = Tensor4D::alloc_all_same(device, &cudnn_handle, &T::ONE)?;
+        let running_mean = Tensor4D::alloc_all_same(device, cudnn_handle, &T::ZERO)?;
+        let running_variance = Tensor4D::alloc_all_same(device, cudnn_handle, &T::ONE)?;
         Ok(Self {
-            cudnn_handle,
-            x,
-            y,
             bias,
             scale,
             saved_mean,
@@ -115,33 +98,31 @@ impl<T: TensorDataType, const N: usize, const C: usize, const H: usize, const W:
         })
     }
 
-    pub fn get_backward(
-        &self,
-        dx: Tensor4D<T, N, C, H, W>,
-        dy: Tensor4D<T, N, C, H, W>,
-    ) -> BatchNormalizationBackward<T, N, C, H, W> {
+    pub fn get_backward(&self) -> BatchNormalizationBackward<T, N, C, H, W> {
         BatchNormalizationBackward::create(
-            Rc::clone(&self.cudnn_handle),
-            self.x.clone(),
-            dx,
-            dy,
             self.scale.clone(),
             self.saved_mean.clone(),
             self.saved_variance.clone(),
         )
     }
 
-    pub fn execute(&self, exponential_average_factor: f64) -> CudnnResult<()> {
+    pub fn execute(
+        &self,
+        cudnn_handle: &CudnnHandle,
+        x: &Tensor4D<T, N, C, H, W>,
+        y: &mut Tensor4D<T, N, C, H, W>,
+        exponential_average_factor: f64,
+    ) -> CudnnResult<()> {
         unsafe {
             cudnnBatchNormalizationForwardTraining(
-                self.cudnn_handle.0,
+                cudnn_handle.0,
                 MODE,
                 &T::ONE as *const _ as *const _,
                 &T::ZERO as *const _ as *const _,
-                self.x.descriptor.0,
-                self.x.data.t_cuda.cu_device_ptr as *const _,
-                self.y.descriptor.0,
-                self.y.data.t_cuda.cu_device_ptr as *mut _,
+                x.descriptor.0,
+                x.data.t_cuda.cu_device_ptr as *const _,
+                y.descriptor.0,
+                y.data.t_cuda.cu_device_ptr as *mut _,
                 self.scale.descriptor.0,
                 self.scale.data.t_cuda.cu_device_ptr as *const _,
                 self.bias.data.t_cuda.cu_device_ptr as *const _,
@@ -159,30 +140,27 @@ impl<T: TensorDataType, const N: usize, const C: usize, const H: usize, const W:
 
 #[cfg(test)]
 mod tests {
-    use alloc::rc::Rc;
-
     use crate::prelude::*;
 
     #[test]
     fn test_batch_normalization() {
         let device = CudaDeviceBuilder::new(0).build().unwrap();
-        let cudnn_handle = Rc::new(CudnnHandle::create(&device).unwrap());
+        let cudnn_handle = CudnnHandle::create(&device).unwrap();
 
         let x = Tensor4D::alloc_with(&device, [[[[2.0, 0.0]], [[-2.0, -6.0]]]]).unwrap();
-        let dx = unsafe { Tensor4D::alloc_uninit(&device).unwrap() };
-        let y = unsafe { Tensor4D::alloc_uninit(&device).unwrap() };
+        let mut dx = unsafe { Tensor4D::alloc_uninit(&device).unwrap() };
+        let mut y = unsafe { Tensor4D::alloc_uninit(&device).unwrap() };
 
-        let forward =
-            BatchNormalizationForward::create(&device, cudnn_handle, x.clone(), y.clone()).unwrap();
-        let backward = forward.get_backward(dx.clone(), y.clone());
+        let forward = BatchNormalizationForward::create(&device, &cudnn_handle).unwrap();
+        let backward = forward.get_backward();
 
-        forward.execute(1.0).unwrap();
+        forward.execute(&cudnn_handle, &x, &mut y, 1.0).unwrap();
 
-        assert_eq!(&*y.data.into_host().unwrap(), &[[[[1.0, -1.0]], [[
-            1.0, -1.0
-        ]]]]);
+        assert_eq!(&*y.data.clone().into_host().unwrap(), &[[[[1.0, -1.0]], [
+            [1.0, -1.0]
+        ]]]);
 
-        backward.execute().unwrap();
+        backward.execute(&cudnn_handle, &x, &mut dx, &y).unwrap();
 
         // TODO seems to be wrong
         std::println!("{:?}", dx.data.into_host());
