a1e49a56ef2890dfa8854e3fe4887d5ae5f112f4
Corey Lowman <coreylowman@users.noreply.github.com>
2023-02-14 11:33:09 -0500

    Adding feature to skip finding cuda/linking (#72)

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 fn main() {
-    find_cuda_helper::include_cuda();
-    println!("cargo:rustc-link-lib=dylib=nvrtc");
-    println!("cargo:rustc-link-lib=dylib=curand");
-    println!("cargo:rustc-link-lib=dylib=cudart");
-    println!("cargo:rustc-link-lib=dylib=cublas");
-    println!("cargo:rustc-link-lib=dylib=cublasLt");
+    #[cfg(not(feature = "ci-check"))]
+    {
+        find_cuda_helper::include_cuda();
+        println!("cargo:rustc-link-lib=dylib=nvrtc");
+        println!("cargo:rustc-link-lib=dylib=curand");
+        println!("cargo:rustc-link-lib=dylib=cudart");
+        println!("cargo:rustc-link-lib=dylib=cublas");
+        println!("cargo:rustc-link-lib=dylib=cublasLt");
+    }
 }
