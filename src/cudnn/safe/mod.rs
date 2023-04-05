46eb4526a0637e565b43f90600117c3bb4e2e13e
Corey Lowman <clowman1993@gmail.com>
2023-04-05 19:09:17 +0000

    Adding safe reductions to cudnn

--- src/cudnn/safe/mod.rs
+++ src/cudnn/safe/mod.rs
@@ -1,5 +1,7 @@
 //! Safe wrappers around cuDNN.
 //!
+//! # Convolutions
+//!
 //! 1. Allocate tensor descriptors with [`Cudnn::create_4d_tensor()`]
 //! 2. Allocate filter descriptors with [`Cudnn::create_4d_filter()`]
 //! 3. Allocate conv descriptors with [`Cudnn::create_conv2d()`]
@@ -11,14 +13,18 @@
 //! 6. Call the `get_workspace_size` method of the struct.
 //! 7. Re-allocate the workspace to the appropriate size.
 //! 8. Call the `launch` method of the struct.
+//!
+//! # Reductions
 
 mod conv;
 mod core;
+mod reduce;
 
 pub use self::conv::{
     Conv2dBackwardData, Conv2dBackwardFilter, Conv2dDescriptor, Conv2dForward, FilterDescriptor,
 };
 pub use self::core::{Cudnn, CudnnDataType, TensorDescriptor};
+pub use self::reduce::{FlatIndices, NoIndices, ReduceTensor, ReductionDescriptor};
 pub use super::result::CudnnError;
 
 #[cfg(test)]
@@ -34,6 +40,14 @@ mod tests {
         let _ = cudnn.create_4d_filter::<f32>(
             cudnn::sys::cudnnTensorFormat_t::CUDNN_TENSOR_NCHW,
             [3, 3, 3, 3],
+        )?;
+        let _ = cudnn.create_reduction_flat_indices::<f32>(
+            cudnn::sys::cudnnReduceTensorOp_t::CUDNN_REDUCE_TENSOR_ADD,
+            cudnn::sys::cudnnNanPropagation_t::CUDNN_PROPAGATE_NAN,
+        )?;
+        let _ = cudnn.create_reduction_no_indices::<f32>(
+            cudnn::sys::cudnnReduceTensorOp_t::CUDNN_REDUCE_TENSOR_ADD,
+            cudnn::sys::cudnnNanPropagation_t::CUDNN_PROPAGATE_NAN,
         )?;
         Ok(())
     }
@@ -104,5 +118,43 @@ mod tests {
         }
 
         Ok(())
+    }
+
+    #[test]
+    fn test_reduction() {
+        let dev = CudaDevice::new(0).unwrap();
+        let a = dev
+            .htod_copy(std::vec![1.0f32, 2.0, 3.0, 4.0, 5.0, 6.0])
+            .unwrap();
+        let mut c = dev.alloc_zeros::<f32>(1).unwrap();
+
+        let cudnn = Cudnn::new(dev.clone()).unwrap();
+
+        let reduce = cudnn
+            .create_reduction_no_indices::<f32>(
+                cudnn::sys::cudnnReduceTensorOp_t::CUDNN_REDUCE_TENSOR_ADD,
+                cudnn::sys::cudnnNanPropagation_t::CUDNN_PROPAGATE_NAN,
+            )
+            .unwrap();
+        let a_desc = cudnn
+            .create_nd_tensor::<f32>(&[1, 1, 2, 3], &[0, 6, 3, 1])
+            .unwrap();
+        let c_desc = cudnn
+            .create_nd_tensor::<f32>(&[1, 1, 1, 1], &[0, 0, 0, 1])
+            .unwrap();
+        let op = ReduceTensor {
+            reduce: &reduce,
+            a: &a_desc,
+            c: &c_desc,
+        };
+
+        let workspace_size = op.get_workspace_size().unwrap();
+        let mut workspace = dev.alloc_zeros::<u8>(workspace_size).unwrap();
+
+        unsafe { op.launch(&mut workspace, (1.0, 0.0), &a, &mut c) }.unwrap();
+
+        let c_host = dev.sync_reclaim(c).unwrap();
+        assert_eq!(c_host.len(), 1);
+        assert_eq!(c_host[0], 21.0);
     }
 }
