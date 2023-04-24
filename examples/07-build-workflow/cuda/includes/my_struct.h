beb35342df7387bcae2481a60b60acfa95777825
l3utterfly <gc.pthzfoldr@gmail.com>
2023-04-24 23:40:55 +0800

    added complex build workflow example (#131)
    Co-authored-by: George <george@mmocircles.com>

---
+++ examples/07-build-workflow/cuda/includes/my_struct.h
@@ -0,0 +1,11 @@
+// my_struct.h
+#ifndef MY_STRUCT_H
+#define MY_STRUCT_H
+
+struct MyStruct
+{
+    /// example data containing fixed length array of floats
+    int data[4];
+};
+
+#endif // MY_STRUCT_H
