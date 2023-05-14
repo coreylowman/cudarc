2403cf6f68e8d13f69f23be52a90c74f453d1be0
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-05-14 18:47:20 -0400

    fixed thread index, added timing, timed example

--- examples/08-rust-kernel.rs
+++ examples/08-rust-kernel.rs
@@ -1,43 +1,69 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, array};
 
 use cudarc::{
     driver::{CudaDevice, DriverError, LaunchAsync, LaunchConfig},
     nvrtc::PtxCrate,
 };
 
+use std::time::Instant;
+
 fn main() -> Result<(), DriverError> {
     let dev = CudaDevice::new(0)?;
 
-    // use compile_crate_to_ptx to build and rust kernels in pure rust
+    // use compile_crate_to_ptx to build kernels in pure Rust
     // uses experimental ABI_PTX
     let kernel_path: PathBuf = "examples/rust-kernel/src/lib.rs".into();
     let kernels = PtxCrate::compile_crate_to_ptx(&kernel_path).unwrap();
     let kernel = kernels.first().unwrap();
 
     // load the ptx file...
-    println!("loading...");
     dev.load_ptx(kernel.clone(), "rust_kernel", &["square_kernel"])?;
-    println!("loaded!");
-
+    
     // and then retrieve the function with `get_func`
     let f = dev.get_func("rust_kernel", "square_kernel").unwrap();
 
-    let a_host = [1.0f32, 2., 3.];
+    const GRID: u32 = 32;
+    const BLOCK: u32 = 512;
+    const THREADS: u32 = GRID * BLOCK;
+
+    // https://developer.nvidia.com/blog/cuda-pro-tip-occupancy-api-simplifies-launch-configuration/
+    let cfg = LaunchConfig {
+        grid_dim: (GRID, 1, 1),
+        block_dim: (BLOCK, 1, 1),
+        shared_mem_bytes: 0,
+    };
+    // or use let cfg = LaunchConfig::for_num_elems(n);
+
+    let a_host: [f32; THREADS as usize] = array::from_fn(|i| {
+        i as f32
+    });
 
     let a_dev = dev.htod_copy(a_host.into())?;
     let mut b_dev = a_dev.clone();
 
-    let n = 3;
-    let cfg = LaunchConfig::for_num_elems(n);
-    unsafe { f.launch(cfg, (&a_dev, &mut b_dev, n as i32)) }?;
-
-    let a_host_2 = dev.sync_reclaim(a_dev.clone())?;
-    let b_host = dev.sync_reclaim(b_dev.clone())?;
-
-    println!("a_host {a_host:?}");
-    println!("b_host {b_host:?}");
-    println!("a_host_2 {a_host_2:?}");
+    println!("squaring 0.0, ..., {}.0 with cuda kernels...", THREADS - 1);
     
+    let start = Instant::now();
+    unsafe { f.launch(cfg, (&a_dev, &mut b_dev, THREADS as i32)) }?;
+    let duration = start.elapsed();
+    println!("launch duration: {:?}", duration);
+    
+    let start = Instant::now();
+    let b_host = dev.sync_reclaim(b_dev.clone())?;
+    let duration = start.elapsed();
+    println!("duration to get results: {:?}", duration);
+    
+    
+    let a_host_2 = dev.sync_reclaim(a_dev.clone())?;
+    let results: Vec<_> = a_host_2.into_iter().zip(b_host.into_iter()).collect();
+    
+    println!("checking some results:");
+    let some_indices: [u32; 6] = [0, THREADS/10, THREADS/5, THREADS/4, THREADS/2, THREADS - 1];
+    for i in some_indices {
+        let (x, x_square) = results.get(i as usize).unwrap();
+        println!("\t{x}^2\t = {x_square}")
+    }
+
     // we can also manage and clean up the build ptx files with a PtxCrate
     let mut rust_ptx: PtxCrate = kernel_path.try_into().unwrap();
     rust_ptx.build_ptx().unwrap();
--- examples/rust-kernel/src/lib.rs
+++ examples/rust-kernel/src/lib.rs
@@ -18,16 +18,23 @@ fn my_panic(_: &core::panic::PanicInfo) -> ! {
 */
 
 #[no_mangle]
-pub unsafe extern "ptx-kernel" fn square_kernel(input: *const f32, output: *mut f32, size: u32) {
+pub unsafe extern "ptx-kernel" fn square_kernel(input: *const f32, output: *mut f32, size: i32) {
     /* https://doc.rust-lang.org/stable/core/arch/nvptx/index.html */
     let thread_id: i32 = _thread_idx_x();
     let block_id: i32 = _block_idx_x();
-    let grid_dim: i32 = _grid_dim_x();
-    let n_threads: i32 = _block_dim_x() * _grid_dim_x();
     
-    let index = thread_id as usize;
-    if index < size as usize {
-        let value = device::square(*input.offset(index as isize));
-        *output.offset(index as isize) = value;
+    let block_dim: i32 = _block_dim_x();
+    let grid_dim: i32 = _grid_dim_x();
+    
+    let n_threads = (block_dim * grid_dim) as u64;
+    
+    let thread_index = 
+        thread_id + 
+        block_id * block_dim
+    ;
+
+    if thread_index < size {
+        let value = device::square(*input.offset(thread_index as isize));
+        *output.offset(thread_index as isize) = value;
     }
 }
