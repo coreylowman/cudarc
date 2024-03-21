de96b8cadfed85bd609120d1ffa2db233590a851
Corey <clowman1993@gmail.com>
2024-03-21 15:37:54 +0000

    Adding feature flags for new supported cuda toolkit verions

--- src/cudnn/sys/mod.rs
+++ src/cudnn/sys/mod.rs
@@ -3,6 +3,16 @@ mod sys_11080;
 #[cfg(feature = "cuda_version_11_8")]
 pub use sys_11080::*;
 
+#[cfg(feature = "cuda_version_12_0")]
+mod sys_12000;
+#[cfg(feature = "cuda_version_12_0")]
+pub use sys_12000::*;
+
+#[cfg(feature = "cuda_version_12_1")]
+mod sys_12010;
+#[cfg(feature = "cuda_version_12_1")]
+pub use sys_12010::*;
+
 #[cfg(feature = "cuda_version_12_2")]
 mod sys_12020;
 #[cfg(feature = "cuda_version_12_2")]
