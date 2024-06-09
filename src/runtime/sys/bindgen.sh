490547b425e0642bb480eab71e209243dfbccf32
psmyth94 <psmyth1994@gmail.com>
2024-06-09 15:30:04 -0500

    modify bindings

--- src/runtime/sys/bindgen.sh
+++ src/runtime/sys/bindgen.sh
@@ -3,23 +3,18 @@
 set -exu
 
 bindgen \
-  --allowlist-type="^CU.*" \
-  --allowlist-type="^cuuint(32|64)_t" \
-  --allowlist-type="^cudaError_enum" \
-  --allowlist-type="^cu.*Complex$" \
-  --allowlist-type="^cuda.*" \
-  --allowlist-type="^libraryPropertyType.*" \
-  --allowlist-var="^CU.*" \
-  --allowlist-function="^cu.*" \
+  --allowlist-type "^cuda.*" \
+  --allowlist-type "^surfaceReference" \
+  --allowlist-type "^textureReference" \
+  --allowlist-var "^cuda.*" \
+  --allowlist-function "^cuda.*" \
   --default-enum-style=rust \
   --no-doc-comments \
   --with-derive-default \
   --with-derive-eq \
   --with-derive-hash \
   --with-derive-ord \
-  --use-core \
-  --dynamic-loading Lib \
-  wrapper.h -- -I$CONDA_PREFIX/targets/x86_64-linux/include \
+  wrapper.h -- -/usr/local/cuda/include \
   > tmp.rs
 
 CUDART_VERSION=$(cat tmp.rs | grep "CUDART_VERSION" | awk '{ print $6 }' | sed 's/.$//')
