161722b0930238415f5c25e556a6bf6966c4f3ec
psmyth94 <psmyth1994@gmail.com>
2024-06-11 16:51:47 -0500

    add cuda_runtime.h in wrapper and bind objects with both CUDA and cuda in their name

--- src/runtime/sys/bindgen.sh
+++ src/runtime/sys/bindgen.sh
@@ -3,18 +3,18 @@
 set -exu
 
 bindgen \
-  --allowlist-type "^cuda.*" \
-  --allowlist-type "^surfaceReference" \
-  --allowlist-type "^textureReference" \
-  --allowlist-var "^cuda.*" \
-  --allowlist-function "^cuda.*" \
+  --allowlist-type "^[Cc][Uu][Dd][Aa].*" \
+  --allowlist-var "^[Cc][Uu][Dd][Aa].*" \
+  --allowlist-function "^[Cc][Uu][Dd][Aa].*" \
   --default-enum-style=rust \
   --no-doc-comments \
   --with-derive-default \
   --with-derive-eq \
   --with-derive-hash \
   --with-derive-ord \
-  wrapper.h -- -/usr/local/cuda/include \
+  --use-core \
+  --dynamic-loading Lib \
+  wrapper.h -- -I/usr/local/cuda/include \
   > tmp.rs
 
 CUDART_VERSION=$(cat tmp.rs | grep "CUDART_VERSION" | awk '{ print $6 }' | sed 's/.$//')
--- src/runtime/sys/wrapper.h
+++ src/runtime/sys/wrapper.h
@@ -1,2 +1,2 @@
-#include "cuda_runtime_api.h"
 #include "cuda_runtime.h"
+#include "cuda_runtime_api.h"
