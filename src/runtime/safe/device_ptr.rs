eb371418d637b29151891a4a94f1bdc2f23f3f58
psmyth94 <psmyth1994@gmail.com>
2024-07-01 12:22:33 -0500

    default of device_ptr is *mut and not *const

--- src/runtime/safe/device_ptr.rs
+++ src/runtime/safe/device_ptr.rs
@@ -1,5 +1,7 @@
 use super::core::{CudaSlice, CudaView, CudaViewMut};
 
+use core::ffi::c_void;
+
 pub trait DeviceSlice<T> {
     fn len(&self) -> usize;
     fn num_bytes(&self) -> usize {
@@ -30,40 +32,40 @@ impl<'a, T> DeviceSlice<T> for CudaViewMut<'a, T> {
 
 /// Abstraction over [CudaSlice]/[CudaView]
 pub trait DevicePtr<T>: DeviceSlice<T> {
-    fn device_ptr(&self) -> *const T;
+    fn device_ptr(&self) -> *const c_void;
 }
 
 impl<T> DevicePtr<T> for CudaSlice<T> {
-    fn device_ptr(&self) -> *const T {
-        self.device_ptr
+    fn device_ptr(&self) -> *const c_void {
+        self.device_ptr as *const c_void
     }
 }
 
 impl<'a, T> DevicePtr<T> for CudaView<'a, T> {
-    fn device_ptr(&self) -> *const T {
-        self.ptr
+    fn device_ptr(&self) -> *const c_void {
+        self.ptr as *const c_void
     }
 }
 
 impl<'a, T> DevicePtr<T> for CudaViewMut<'a, T> {
-    fn device_ptr(&self) -> *const T {
-        self.ptr
+    fn device_ptr(&self) -> *const c_void {
+        self.ptr as *const c_void
     }
 }
 
 /// Abstraction over [CudaSlice]/[CudaViewMut]
 pub trait DevicePtrMut<T>: DeviceSlice<T> {
-    fn device_ptr_mut(&mut self) -> *mut T;
+    fn device_ptr_mut(&mut self) -> *mut c_void;
 }
 
 impl<T> DevicePtrMut<T> for CudaSlice<T> {
-    fn device_ptr_mut(&mut self) -> *mut T {
+    fn device_ptr_mut(&mut self) -> *mut c_void {
         self.device_ptr
     }
 }
 
 impl<'a, T> DevicePtrMut<T> for CudaViewMut<'a, T> {
-    fn device_ptr_mut(&mut self) -> *mut T {
+    fn device_ptr_mut(&mut self) -> *mut c_void {
         self.ptr
     }
 }
