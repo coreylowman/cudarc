a749c6c1bd1bd6d08c0d03cadb63b34585f6c7cc
wcork <willfcork@gmail.com>
2025-02-08 20:49:23 -0800

    feat(add cuda-12080)
    Bindgen is a bit more dynamic as well

--- src/nccl/sys/bindgen.sh
+++ src/nccl/sys/bindgen.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -exu
 BINDGEN_EXTRA_CLANG_ARGS="-D__CUDA_BF16_TYPES_EXIST__" \
-bindgen \
-  --allowlist-var="^CUDA_VERSION" \
-  --allowlist-type="^nccl.*" \
-  --allowlist-var="^nccl.*" \
-  --allowlist-function="^nccl.*" \
-  --default-enum-style=rust \
-  --no-doc-comments \
-  --with-derive-default \
-  --with-derive-eq \
-  --with-derive-hash \
-  --with-derive-ord \
-  --use-core \
-  --dynamic-loading Lib \
-  wrapper.h -- -I/usr/local/cuda/include -I/usr/lib/gcc/x86_64-linux-gnu/12/include/ \
-  > tmp.rs
+    bindgen \
+    --allowlist-var="^CUDA_VERSION" \
+    --allowlist-type="^nccl.*" \
+    --allowlist-var="^nccl.*" \
+    --allowlist-function="^nccl.*" \
+    --default-enum-style=rust \
+    --no-doc-comments \
+    --with-derive-default \
+    --with-derive-eq \
+    --with-derive-hash \
+    --with-derive-ord \
+    --use-core \
+    --dynamic-loading Lib \
+    wrapper.h -- -I$CUDA_INCLUDES -I/usr/lib/gcc/x86_64-linux-gnu/12/include/ \
+    >tmp.rs
 
 CUDA_VERSION=$(cat tmp.rs | grep "CUDA_VERSION" | awk '{ print $6 }' | sed 's/.$//')
 mv tmp.rs sys_${CUDA_VERSION}.rs
--- src/runtime/sys/bindgen.sh
+++ src/runtime/sys/bindgen.sh
@@ -3,19 +3,19 @@
 set -exu
 
 bindgen \
-  --allowlist-type "^[Cc][Uu][Dd][Aa].*" \
-  --allowlist-var "^[Cc][Uu][Dd][Aa].*" \
-  --allowlist-function "^[Cc][Uu][Dd][Aa].*" \
-  --default-enum-style=rust \
-  --no-doc-comments \
-  --with-derive-default \
-  --with-derive-eq \
-  --with-derive-hash \
-  --with-derive-ord \
-  --use-core \
-  --dynamic-loading Lib \
-  wrapper.h -- -I/usr/local/cuda/include \
-  > tmp.rs
+    --allowlist-type "^[Cc][Uu][Dd][Aa].*" \
+    --allowlist-var "^[Cc][Uu][Dd][Aa].*" \
+    --allowlist-function "^[Cc][Uu][Dd][Aa].*" \
+    --default-enum-style=rust \
+    --no-doc-comments \
+    --with-derive-default \
+    --with-derive-eq \
+    --with-derive-hash \
+    --with-derive-ord \
+    --use-core \
+    --dynamic-loading Lib \
+    wrapper.h -- -I$CUDA_INCLUDES \
+    >tmp.rs
 
 CUDART_VERSION=$(cat tmp.rs | grep "CUDART_VERSION" | awk '{ print $6 }' | sed 's/.$//')
 mv tmp.rs sys_${CUDART_VERSION}.rs
