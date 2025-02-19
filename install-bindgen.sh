f04a31c439d0e0d2e1b747eb5c18417af4f3cd99
Corey Lowman <clowman1993@gmail.com>
2025-02-19 15:01:39 +0000

    Removing layout tests for 12.5

--- install-bindgen.sh
+++ install-bindgen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-apt update -y
-apt upgrade -y
-apt install -y curl clang
+apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y curl clang
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 cargo install bindgen-cli@0.71.1
