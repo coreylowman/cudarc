a40014d341e814f2f0a093111397c6f7d1f6a408
Corey Lowman <clowman1993@gmail.com>
2025-06-27 13:44:42 +0000

    #411 Fixing cusolvermg dynamic-linking & static-linking

--- build.rs
+++ build.rs
@@ -143,14 +143,16 @@ fn dynamic_linking(major: usize, minor: usize) {
     println!("cargo:rustc-link-lib=dylib=nvrtc");
     #[cfg(feature = "curand")]
     println!("cargo:rustc-link-lib=dylib=curand");
-    #[cfg(any(feature = "cublas", feature = "cusolver"))]
+    #[cfg(any(feature = "cublas"))]
     println!("cargo:rustc-link-lib=dylib=cublas");
     #[cfg(any(feature = "cublas", feature = "cublaslt"))]
     println!("cargo:rustc-link-lib=dylib=cublasLt");
-    #[cfg(any(feature = "cusparse", feature = "cusolver"))]
+    #[cfg(any(feature = "cusparse"))]
     println!("cargo:rustc-link-lib=dylib=cusparse");
     #[cfg(feature = "cusolver")]
     println!("cargo:rustc-link-lib=dylib=cusolver");
+    #[cfg(feature = "cusolver")]
+    println!("cargo:rustc-link-lib=dylib=cusolverMg");
     #[cfg(feature = "cudnn")]
     println!("cargo:rustc-link-lib=dylib=cudnn");
     #[cfg(feature = "runtime")]
@@ -187,14 +189,19 @@ fn static_linking(major: usize, minor: usize) {
     println!("cargo:rustc-link-lib=static:+whole-archive=culibos");
     #[cfg(feature = "curand")]
     println!("cargo:rustc-link-lib=static:+whole-archive=curand_static");
-    #[cfg(any(feature = "cublas", feature = "cusolver"))]
+    #[cfg(any(feature = "cublas"))]
     println!("cargo:rustc-link-lib=static:+whole-archive=cublas_static");
     #[cfg(any(feature = "cublas", feature = "cublaslt"))]
     println!("cargo:rustc-link-lib=static:+whole-archive=cublasLt_static");
-    #[cfg(any(feature = "cusparse", feature = "cusolver"))]
+    #[cfg(any(feature = "cusparse"))]
     println!("cargo:rustc-link-lib=static:+whole-archive=cusparse_static");
     #[cfg(feature = "cusolver")]
-    println!("cargo:rustc-link-lib=static:+whole-archive=cusolver_static");
+    {
+
+        println!("cargo:rustc-link-lib=static:+whole-archive=cusolver_static");
+        println!("cargo:rustc-link-lib=static:+whole-archive=cusolver_lapack_static");
+        println!("cargo:rustc-link-lib=static:+whole-archive=cusolver_metis_static");
+    }
     #[cfg(feature = "cudnn")]
     println!("cargo:rustc-link-lib=static:+whole-archive=cudnn");
 }
