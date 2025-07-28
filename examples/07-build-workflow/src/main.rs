111b70e8af3328846e89736648e5f9d0788f3a3b
Corey Lowman <clowman1993@gmail.com>
2025-07-28 17:43:14 +0000

    Fix example 7

--- examples/07-build-workflow/src/main.rs
+++ examples/07-build-workflow/src/main.rs
@@ -13,7 +13,7 @@
 //! There are two files in this example: `main.rs` and `build.rs`. You can reference them and add to your project accordingly. The `cuda` folder in this example gives a simple example of defining structs in a separate header, including creating a `wrapper.h` header for `bindgen`
 
 use std::time::Instant;
-use cudarc::driver::{CudaDevice, LaunchConfig, DeviceRepr, DriverError, LaunchAsync};
+use cudarc::driver::*;
 use cudarc::nvrtc::Ptx;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
@@ -32,15 +32,16 @@ fn main() -> Result<(), DriverError> {
     // setup GPU device
     let now = Instant::now();
 
-    let gpu = CudaDevice::new(0)?;
+    let ctx = CudaContext::new(0)?;
+    let stream = ctx.default_stream();
 
     println!("Time taken to initialise CUDA: {:.2?}", now.elapsed());
 
     // compile ptx
     let now = Instant::now();
 
-    let ptx = Ptx::from_src(CUDA_KERNEL_MY_STRUCT);
-    gpu.load_ptx(ptx, "my_module", &["my_struct_kernel"])?;
+    let my_module = ctx.load_module(Ptx::from_src(CUDA_KERNEL_MY_STRUCT))?;
+    let my_function = my_module.load_function("my_struct_kernel")?;
 
     println!("Time taken to compile and load PTX: {:.2?}", now.elapsed());
 
@@ -51,19 +52,20 @@ fn main() -> Result<(), DriverError> {
     let my_structs = vec![MyStruct { data: [1.0; 4] }; n];
 
     // copy to GPU
-    let gpu_my_structs = gpu.htod_copy(my_structs)?;
+    let mut gpu_my_structs = stream.memcpy_stod(&my_structs)?;
 
     println!("Time taken to initialise data: {:.2?}", now.elapsed());
 
     let now = Instant::now();
-
-    let f = gpu.get_func("my_module", "my_struct_kernel").unwrap();
-
-    unsafe { f.launch(LaunchConfig::for_num_elems(n as u32), (&gpu_my_structs, n)) }?;
+    let mut launch_args = stream.launch_builder(&my_function);
+    launch_args.arg(&mut gpu_my_structs);
+    launch_args.arg(&n);
+    let cfg = LaunchConfig::for_num_elems(n as u32);
+    unsafe { launch_args.launch(cfg) }?;
 
     println!("Time taken to call kernel: {:.2?}", now.elapsed());
 
-    let my_structs = gpu.sync_reclaim(gpu_my_structs)?;
+    let my_structs = stream.memcpy_dtov(&gpu_my_structs)?;
 
     assert!(my_structs.iter().all(|i| i.data == [2.0; 4]));
 
