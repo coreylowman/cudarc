b6560e260cab615e5195d13bf9bf96d64d7fd034
Corey Lowman <clowman1993@gmail.com>
2025-08-05 18:01:42 +0000

    compile error with cuda-13000 and cusolver/cusolvermg

--- src/cusolver/mod.rs
+++ src/cusolver/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "cuda-13000")]
+compile_error!("cusolver doesn't support cuda 13.0 yet, please open a PR once support has been added by cusolver team");
+
 #[allow(warnings)]
 pub mod sys;
 
--- src/cusolvermg/mod.rs
+++ src/cusolvermg/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "cuda-13000")]
+compile_error!("cusolvermg doesn't support cuda 13.0 yet, please open a PR once support has been added by cusolvermg team");
+
 pub mod result;
 pub mod safe;
 #[allow(warnings)]
