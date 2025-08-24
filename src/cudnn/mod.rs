139f5c517fca3df9bd615c50b51ebf56197c2d0e
Ilya Kryukov <ilyakrukov@gmail.com>
2025-08-24 18:24:50 +0200

    Remove compile error for unsupported CUDA 13.0 in cudnn module

--- src/cudnn/mod.rs
+++ src/cudnn/mod.rs
@@ -1,6 +1,3 @@
-#[cfg(feature = "cuda-13000")]
-compile_error!("cudnn doesn't support cuda 13.0 yet, please open a PR once support has been added by cudnn team");
-
 pub mod result;
 pub mod safe;
 #[allow(warnings)]
