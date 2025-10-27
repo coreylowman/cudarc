8ff8a5ce0d2910883e29590e3e9d576e077056ff
Corey Lowman <clowman1993@gmail.com>
2025-10-27 13:03:52 -0400

    Add top level mod docs

--- src/nvtx/mod.rs
+++ src/nvtx/mod.rs
@@ -1,4 +1,16 @@
-//! TODO
+//! Safe wrapper for [nvtx](https://nvidia.github.io/NVTX/) apis. Use [`scoped_range()`], [`mark()`], or [`Event`].
+//! 
+/// Example [`mark()`]/[`Event::mark()`] usage:
+/// ```no_run
+/// Event::message("Hello world").mark();
+/// ```
+///
+/// Example [`scoped_range()`]/[`Event::range()`] usage:
+/// ```no_run
+/// let range = Event::message("Hello_world").argb(0xffff0000).range();
+/// // ... stuff you want to mark
+/// drop(range);
+/// ```
 
 pub mod result;
 pub mod safe;
